139a343d293465af56b451046fc4bf2decb95099
Artur H. Lange <artur@arc2.dev>
2024-04-06 13:10:50 +0200

    Remove unnecessary stuff

--- unzip.sh
+++ unzip.sh
@@ -3,6 +3,4 @@
 FILE_LOCATION=/tmp/$FEED_NAME/original
 FEED_FILE=$(ls $FILE_LOCATION | sort -r | head -1)
 
-echo "GTFS_FILE=$FEED_FILE" >> "$GITHUB_OUTPUT"
-
-unzip -u $FILE_LOCATION/$FEED_FILE -d feed/
+unzip -o $FILE_LOCATION/$FEED_FILE -d feed/
